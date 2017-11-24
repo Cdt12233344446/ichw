28f917269545fb19ab7d342b3fdea0c45d595f7d
Cdt12233344446 <Cdt12233344446@163.com>
2017-11-24 11:33:49 +0800

    Fixed previous update. Added exception handle.

--- pyassign1/planets.py
+++ pyassign1/planets.py
@@ -11,7 +11,7 @@ __email__  = "1700011774@pku.edu.cn"
 
 import math as m
 import turtle
-import random
+import random as ran
 
 # defines objects
 turtle.setup(width=1.0, height=1.0, startx=None, starty=None)
@@ -29,14 +29,22 @@ Sun.shape("circle")
 Sun.shapesize(1.09, 1.09, 0)
 Sun.color("yellow", "yellow")
 
-"""the keys 'b' and 'c' are geometric factors
-   created to simplify the expressions in main() and will be evaluated below
+"""
+A list of parameters needed for the following codes. Here
+'i' is the index or 'self' parameter,
+'rad' determines the size of the planet,
+'col' is the designated color,
+'φ' is the 'phase' that will be randomized later,
+'a' is the semimajor axis,
+'e' defines the eccentricity of the orbit,
+'b' and 'c' are auxillary and will be assigned values immediately after.
 """
 planets_and_parameters = [
     {'i': Mercury,
      'rad': 1.53,
      'per': 24.1,
      'col': "purple",
+     'φ': 0,
      'a': 5.79,
      'e': 0.206,
      'b': 0,
@@ -47,6 +55,7 @@ planets_and_parameters = [
      'rad': 3.80,
      'per': 61.5,
      'col': "orange",
+     'φ': 0,
      'a': 10.82,
      'e': 0.007,
      'b': 0,
@@ -57,6 +66,7 @@ planets_and_parameters = [
      'rad': 4.00,
      'per': 100.0,
      'col': "blue",
+     'φ': 0,
      'a': 14.96,
      'e': 0.016,
      'b': 0,
@@ -67,6 +77,7 @@ planets_and_parameters = [
      'rad': 2.13,
      'per': 188.0,
      'col': "red",
+     'φ': 0,
      'a': 22.79,
      'e': 0.093,
      'b': 0,
@@ -77,6 +88,7 @@ planets_and_parameters = [
      'rad': 11.2,
      'per': 1186,
      'col': "green",
+     'φ': 0,
      'a': 77.84,
      'e': 0.048,
      'b': 0,
@@ -87,6 +99,7 @@ planets_and_parameters = [
      'rad': 9.45,
      'per': 2945,
      'col': "brown",
+     'φ': 0,
      'a': 142.67,
      'e': 0.054,
      'b': 0,
@@ -95,36 +108,47 @@ planets_and_parameters = [
 
     ]
 
-# sends planets to their initial position
-for pls in planets_and_parameters:
-    pls['i'].shape("circle")
-    pls['i'].shapesize(pls['rad'] / 20, pls['rad'] / 20, 0)
-    pls['i'].color(pls['col'], pls['col'])
-    pls['b'] = pls['a'] * m.sqrt(1 - pls['e'] ** 2)
-    pls['c'] = pls['a'] * pls['e']
-    pls['i'].up()
-    pls['i'].goto(pls['a'] * (1 - pls['e']) * 4, 0)
-    pls['i'].down()
+
+def init():
+    """sends planets to their initial position
+    """
+    for pls in planets_and_parameters:
+        pls['i'].shape("circle")
+        pls['i'].shapesize(pls['rad'] / 20, pls['rad'] / 20, 0)
+        pls['i'].color(pls['col'], pls['col'])
+        pls['i'].speed(0)
+        pls['φ'] = ran.uniform(-m.pi, m.pi)
+        pls['b'] = pls['a'] * m.sqrt(1 - pls['e'] ** 2)
+        pls['c'] = pls['a'] * pls['e']
+        pls['i'].up()
+        pls['i'].goto(
+            4 * (pls['a'] * m.cos(pls['φ']) - pls['c']),
+            4 * pls['b'] * m.sin(pls['φ']))
+        pls['i'].down()
 
 
 def main():
     """keeps the planets going
     """
     try:
-        for t in range(1, 200):
+        for t in range(1, 2000):
             for pl in planets_and_parameters:
+                # use φ to simplify the expression, again
+                pl['φ'] = pl['φ'] + 2 * m.pi / pl['per']
                 pl['i'].goto(
-                    4 * (pl['a'] * m.cos(2 * m.pi * t / pl['per']) - pl['c']),
-                    4 * pl['b'] * m.sin(2 * m.pi * t / pl['per']))
+                    4 * (pl['a'] * m.cos(pl['φ']) - pl['c']),
+                    4 * pl['b'] * m.sin(pl['φ']))
 
-        # avoids redundant rendering by 'upping' the faster planets
-        if t == 100:
-            for pls in planets_and_parameters[:3]:
-                pls['i'].up()
+                # avoids redundant rendering by 'upping' the faster planets
+                if t == 100:
+                    for pls in planets_and_parameters[:3]:
+                        pls['i'].up()
     except:
-        pass
-    finally:
+        print("Pity that you quit so early. See you next time!")
+    else:
         print("Thanks for watching this simulaton. See you next time!")
 
+
 if __name__ == '__main__':
+    init()
     main()
