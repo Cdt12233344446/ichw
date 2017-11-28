77b8acc3c3bf794fb87b0e7a126c1ecf941849fa
Cdt12233344446 <Cdt12233344446@163.com>
2017-11-28 21:25:12 +0800

    stable version release

--- pyassign2/currency.py
+++ pyassign2/currency.py
@@ -10,6 +10,7 @@ __email__  = "1700011774@pku.edu.cn"
 
 from urllib.request import urlopen
 
+
 def exchange(currency_from, currency_to, amount_from):
     """Returns: amount of currency received in the given exchange.
 
@@ -29,14 +30,71 @@ def exchange(currency_from, currency_to, amount_from):
     Precondition: amount_from is a float
     """
     # append arguments step-by-step to reduce code length
-    url = 'http://cs1110.cs.cornell.edu/2016fa/a1server.php?'
-    url = url + 'from=' + currency_from
+    url = 'http://cs1110.cs.cornell.edu/2016fa/a1server.php'
+    url = url + '?from=' + currency_from
     url = url + '&to=' + currency_to
     url = url + '&amt=' + str(amount_from)
+
+    # open the url and read the return string
     doc = urlopen(url)
     docstr = doc.read()
     doc.close()
-    jstr = docstr.decode('ascii')
-    print(jstr)
 
-exchange('USD', 'EUR', 2.5)
+    # process the return string to 'extract' the amount
+    jstr = docstr.decode('ascii')
+    jstr_split = jstr.split(sep=' ')
+    index = jstr_split.index('"to"')
+    return float(jstr_split[index+2].lstrip('"'))
+
+
+def hasError():
+    """Intended for handling exceptional json strings.
+
+    However, as specified in the Precondition,
+    there's no need to handle errors.Therefore we leave it empty.
+    """
+    return False
+
+
+def testError():
+    """test if errors are handled properly
+    """
+    assert(False == hasError())
+
+
+def test_A():
+    """one of a series of tests, with this one concerning exchange from USD
+    """
+    assert(1 == exchange('USD', 'USD', 1))              # initial test
+    assert(2.0952375 == exchange('USD', 'EUR', 2.5))
+    assert(272.146438625 == exchange('USD', 'JPY', 2.5))
+
+
+def test_B():
+    """one of a series of tests, with this one concerning exchange to USD
+    """
+    assert(2.9829553928851 == exchange('EUR', 'USD', 2.5))
+    assert(0.38307424745064 == exchange('CNY', 'USD', 2.5))
+    assert(0.022965577031166 == exchange('JPY', 'USD', 2.5))
+
+
+def test_C():
+    """one of a series of tests, with this one concerning exchange
+    to and from less-used currencies
+    """
+    assert(4.8955068493151 == exchange('ZMW', 'MXN', 2.5))
+    assert(1.9231929528177 == exchange('THB', 'CUP', 2.5))
+    assert(0.001958975 == exchange('KPW', 'XDR', 2.5))
+
+
+def testAll():
+    """test all cases"""
+    testError()
+    test_A()
+    test_B()
+    test_C()
+    print("All tests passed")
+
+
+if __name__ == '__main__':
+    testAll()
