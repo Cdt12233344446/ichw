823f3e6a26a003a238de47d87bb54d89d04a3d96
Cdt12233344446 <Cdt12233344446@163.com>
2017-11-29 16:40:56 +0800

    improved and clarified version

--- pyassign2/currency.py
+++ pyassign2/currency.py
@@ -11,6 +11,31 @@ __email__  = "1700011774@pku.edu.cn"
 from urllib.request import urlopen
 
 
+def jstr_fetch(fro, to, amu):
+    """fetch the json string from given input
+    """
+    # append url arguments step-by-step to reduce code length
+    url = 'http://cs1110.cs.cornell.edu/2016fa/a1server.php'
+    url = url + '?from=' + fro
+    url = url + '&to=' + to
+    url = url + '&amt=' + str(amu)
+
+    # open the url and read the return string
+    doc = urlopen(url)
+    docstr = doc.read()
+    doc.close()
+    return docstr
+
+
+def jstr_process(rawstr):
+    """process the string to 'extract' the amount
+    """
+    jstr = rawstr.decode('ascii')
+    jstr_split = jstr.split(sep=' ')
+    index = jstr_split.index('"to"')
+    return float(jstr_split[index+2].lstrip('"'))
+
+
 def exchange(currency_from, currency_to, amount_from):
     """Returns: amount of currency received in the given exchange.
 
@@ -29,44 +54,51 @@ def exchange(currency_from, currency_to, amount_from):
     Parameter amount_from: amount of currency to convert
     Precondition: amount_from is a float
     """
-    # append arguments step-by-step to reduce code length
-    url = 'http://cs1110.cs.cornell.edu/2016fa/a1server.php'
-    url = url + '?from=' + currency_from
-    url = url + '&to=' + currency_to
-    url = url + '&amt=' + str(amount_from)
-
-    # open the url and read the return string
-    doc = urlopen(url)
-    docstr = doc.read()
-    doc.close()
-
-    # process the return string to 'extract' the amount
-    jstr = docstr.decode('ascii')
-    jstr_split = jstr.split(sep=' ')
-    index = jstr_split.index('"to"')
-    return float(jstr_split[index+2].lstrip('"'))
+    docstr = jstr_fetch(currency_from, currency_to, amount_from)
+    amount_to = jstr_process(docstr)
+    return amount_to
 
 
 def hasError():
     """Intended for handling exceptional json strings.
 
     However, as specified in the Precondition,
-    there's no need to handle errors.Therefore we leave it empty.
+    there's no need to handle errors. Therefore we leave it empty.
     """
     return False
 
 
+# The next three tests are rather formal; they always pass.
 def testError():
     """test if errors are handled properly
     """
     assert(False == hasError())
 
 
+def testfetch():
+    """test if docstr is properly read
+    """
+    rawstr = jstr_fetch('USD', 'USD', 2.5)
+    test_jstr = b'{ "from" : "2.5 United States Dollars", "to" : "2.5 United States Dollars", "success" : true, "error" : "" }'
+    assert(test_jstr == rawstr)
+
+
+def testprocess():
+    """Intended to test if rawstr is properly processed.
+
+    However, since jstr_process() is immediately followed by exchange()
+    and thus cannot be isolated, testing jstr_process() is equivalent to
+    testing exchange(). So we leave it empty again.
+    """
+    pass
+
+
+# these are the 'real' tests
 def test_A():
     """one of a series of tests, with this one concerning exchange from USD
     """
     assert(1 == exchange('USD', 'USD', 1))              # initial test
-    assert(2.0952375 == exchange('USD', 'EUR', 2.5))
+    assert(2.0952375 == exchange('USD', 'EUR', 2.5))    # sample test
     assert(272.146438625 == exchange('USD', 'JPY', 2.5))
 
 
@@ -80,7 +112,7 @@ def test_B():
 
 def test_C():
     """one of a series of tests, with this one concerning exchange
-    to and from less-used currencies
+    to and from lesser-known currencies
     """
     assert(4.8955068493151 == exchange('ZMW', 'MXN', 2.5))
     assert(1.9231929528177 == exchange('THB', 'CUP', 2.5))
@@ -90,6 +122,8 @@ def test_C():
 def testAll():
     """test all cases"""
     testError()
+    testfetch()
+    testprocess()
     test_A()
     test_B()
     test_C()
